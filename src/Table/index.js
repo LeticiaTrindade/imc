3ee53eb14c8b5497135e9fe80ea19c440fa3d3ea
leticia <leticiatrindade0504@gmail.com>
2023-01-02 19:22:56 -0300

    final

--- src/Table/index.js
+++ src/Table/index.js
@@ -6,44 +6,44 @@ function Table(){
     <div id="table">
         <h2 className="text-center">Tabela IMC</h2>
         <div className="row justify-content-md-center text-center">
-            <h5 className=" col-sm-3">IMC</h5>
-            <h5 className=" col-sm-3">CLASSIFICAÇÃO</h5>
-            <h5 className=" col-sm-3">OBESIDADE(GRAU)</h5>
+            <h5 className=" col-md-3">IMC</h5>
+            <h5 className=" col-md-3">CLASSIFICAÇÃO</h5>
+            <h5 className=" col-md-3">OBESIDADE(GRAU)</h5>
         </div>
         <hr/>
 
         <div className="row justify-content-md-center text-center">
-            <h6 className=" col-sm-3">Menor que 18.5</h6>
-            <h6 className=" col-sm-3">Magreza</h6>
-            <h6 className=" col-sm-3">0</h6>
+            <h6 className=" col-md-3">Menor que 18.5</h6>
+            <h6 className=" col-md-3">Magreza</h6>
+            <h6 className=" col-md-3">0</h6>
         </div>
         <hr/>
 
         <div className="row justify-content-md-center text-center">
-            <h6 className=" col-sm-3">Entre 18.5 e 24.9</h6>
-            <h6 className=" col-sm-3">Normal</h6>
-            <h6 className=" col-sm-3">0</h6>
+            <h6 className=" col-md-3">Entre 18.5 e 24.9</h6>
+            <h6 className=" col-md-3">Normal</h6>
+            <h6 className=" col-md-3">0</h6>
         </div>
         <hr/>
 
         <div className="row justify-content-md-center text-center">
-            <h6 className=" col-sm-3">Entre 25 e 29.9</h6>
-            <h6 className=" col-sm-3">Sobrepeso</h6>
-            <h6 className=" col-sm-3">I</h6>
+            <h6 className=" col-md-3">Entre 25 e 29.9</h6>
+            <h6 className=" col-md-3">Sobrepeso</h6>
+            <h6 className=" col-md-3">I</h6>
         </div>
         <hr/>
 
         <div className="row justify-content-md-center text-center">
-            <h6 className=" col-sm-3">Entre 30 e 39.9</h6>
-            <h6 className=" col-sm-3">Obesidade</h6>
-            <h6 className=" col-sm-3">II</h6>
+            <h6 className=" col-md-3">Entre 30 e 39.9</h6>
+            <h6 className=" col-md-3">Obesidade</h6>
+            <h6 className=" col-md-3">II</h6>
         </div>
         <hr/>
 
         <div className="row justify-content-md-center text-center">
-            <h6 className=" col-sm-3">Maior que 40</h6>
-            <h6 className=" col-sm-3">Obesidade grave</h6>
-            <h6 className=" col-sm-3">III</h6>
+            <h6 className=" col-md-3">Maior que 40</h6>
+            <h6 className=" col-md-3">Obesidade grave</h6>
+            <h6 className=" col-md-3">III</h6>
         </div>
         <hr/>
         </div>
